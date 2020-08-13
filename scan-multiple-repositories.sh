c5d9929639bb05b67e863e341d2072a8bfe80a4f
Gudmundur Haraldsson <gdh1@hi.is>
2020-08-13 13:18:10 +0000

    Adding support for PHPCS runtime set, small changes

--- scan-multiple-repositories.sh
+++ scan-multiple-repositories.sh
@@ -5,17 +5,19 @@ GITHUB_REPOS="$2"
 GITHUB_TOKEN="$3"
 GITHUB_LABELS="$4"
 PHPCS_STANDARDS="$5"
+PHPCS_RUNTIME_SET="$6"
 
-if [ "" == "$GITHUB_ORGANISATION" ] || [ "" == "$GITHUB_REPOS" ] || [ "" == "$GITHUB_TOKEN" ] || [ "" == "$GITHUB_LABELS" ] || [ "" == "$PHPCS_STANDARDS"  ] ; then
+if [ "" == "$GITHUB_ORGANISATION" ] || [ "" == "$GITHUB_REPOS" ] || [ "" == "$GITHUB_TOKEN" ] || [ "" == "$GITHUB_LABELS" ] || [ "" == "$PHPCS_STANDARDS" ] || [ "" == "$PHPCS_RUNTIME_SET" ] ; then
 	echo "Scan multiple repositories with vip-go-compatibility-scanner and note issues found"
 	echo ""
-	echo "Usage: $0 github-organisation github-repos github-token github-labels phpcs-standard"
+	echo "Usage: $0 github-organisation github-repos github-token github-labels phpcs-standard phpcs-runtime-set"
 	echo ""
 	echo "          github-organisation: GitHub organisation repositories belong to"
 	echo "          github-repos: Comma separated string of repositories to scan"
         echo "          github-token: Access token for GitHub"
  	echo "          github-labels: Labels to apply to issues created. Separate by commas for multiple"
 	echo "          phpcs-standards: PHPCS standard(s) to use while scanning. Separate by commas for multiple"
+	echo "          phpcs-runtime-set: Runtime parameter to set, for example: \"testVersion 7.2-\""
 	echo ""
 	exit 1
 fi
@@ -26,6 +28,7 @@ echo "-- GitHub organisation: $GITHUB_ORGANISATION"
 echo "    --    repositories: $GITHUB_REPOS"
 echo "    --    labels to apply: $GITHUB_LABELS"
 echo "-- PHPCS standard(s) to use: $PHPCS_STANDARDS"
+echo "    --   runtime set: $PHPCS_RUNTIME_SET"
 echo ""
 echo -n "Do you want to continue? (Y/N) "
 
@@ -41,7 +44,8 @@ TEMP_DIR=`mktemp -d /tmp/vip-go-compatibility-scanner-run-XXXXXXX`
 GITHUB_REPOS=`echo $GITHUB_REPOS | sed 's/,/ /g'`
 
 for REPO_NAME in $(echo "$GITHUB_REPOS") ; do
-	TEMP_LOG_FILE="$TEMP_DIR/log-$REPO_NAME.txt"
+	TIMESTAMP=`date +%s`
+	TEMP_LOG_FILE="$TEMP_DIR/log-$REPO_NAME-$TIMESTAMP.txt"
 
 	echo "Scanning $REPO_NAME..."
 	echo "Log file will be placed in: $TEMP_LOG_FILE"
@@ -51,7 +55,7 @@ for REPO_NAME in $(echo "$GITHUB_REPOS") ; do
 	git clone "git@github.com:$GITHUB_ORGANISATION/$REPO_NAME.git" && \
 	echo "Running scanner..." && \
 	popd && \
-	./compatibility-scanner.php --repo-owner="$GITHUB_ORGANISATION" --repo-name="$REPO_NAME" --token="$GITHUB_TOKEN" --local-git-repo="$TEMP_DIR/$REPO_NAME" --phpcs-path="$HOME/vip-go-ci-tools/phpcs/bin/phpcs" --phpcs-standard="$PHPCS_STANDARDS"  --vipgoci-path="$HOME/vip-go-ci-tools/vip-go-ci/" --phpcs-runtime-set='testVersion 7.4-' --github-labels="$GITHUB_LABELS" | tee "$TEMP_LOG_FILE" && \
+	./compatibility-scanner.php --repo-owner="$GITHUB_ORGANISATION" --repo-name="$REPO_NAME" --token="$GITHUB_TOKEN" --local-git-repo="$TEMP_DIR/$REPO_NAME" --phpcs-path="$HOME/vip-go-ci-tools/phpcs/bin/phpcs" --phpcs-standard="$PHPCS_STANDARDS"  --vipgoci-path="$HOME/vip-go-ci-tools/vip-go-ci/" --phpcs-runtime-set="$PHPCS_RUNTIME_SET" --github-labels="$GITHUB_LABELS" | tee "$TEMP_LOG_FILE" && \
 	echo "Processing of $REPO_NAME done." && \
 	rm -rf "$TEMP_DIR/$REPO_NAME" && \
 	echo "Removed cloned repository from $TEMP_DIR" && \
