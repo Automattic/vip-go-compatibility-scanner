bfd89911a4117ea4315a0abae227ebd4f5afb137
Gudmundur Haraldsson <gdh1@hi.is>
2020-08-17 16:35:34 +0000

    Issue body in one parameter, support for assigning issues to admins

--- scan-multiple-repositories.sh
+++ scan-multiple-repositories.sh
@@ -5,23 +5,23 @@ GITHUB_REPOS="$2"
 GITHUB_TOKEN="$3"
 GITHUB_LABELS="$4"
 GITHUB_ISSUE_TITLE="$5"
-GITHUB_ISSUE_BODY_INTRO="$6"
-GITHUB_ISSUE_BODY_END="$7"
+GITHUB_ISSUE_BODY="$6"
+GITHUB_ISSUE_ASSIGN="$7"
 PHPCS_STANDARDS="$8"
 PHPCS_RUNTIME_SET="$9"
 
-if [ "" == "$GITHUB_ORGANISATION" ] || [ "" == "$GITHUB_REPOS" ] || [ "" == "$GITHUB_TOKEN" ] || [ "" == "$GITHUB_LABELS" ] || [ "" == "$GITHUB_ISSUE_TITLE" ]|| [ "" == "$GITHUB_ISSUE_BODY_INTRO" ]|| [ "" == "$GITHUB_ISSUE_BODY_END" ]  || [ "" == "$PHPCS_STANDARDS" ] || [ "" == "$PHPCS_RUNTIME_SET" ] ; then
+if [ "" == "$GITHUB_ORGANISATION" ] || [ "" == "$GITHUB_REPOS" ] || [ "" == "$GITHUB_TOKEN" ] || [ "" == "$GITHUB_LABELS" ] || [ "" == "$GITHUB_ISSUE_TITLE" ]|| [ "" == "$GITHUB_ISSUE_BODY" ]|| [ "" == "$GITHUB_ISSUE_ASSIGN" ]  || [ "" == "$PHPCS_STANDARDS" ] || [ "" == "$PHPCS_RUNTIME_SET" ] ; then
 	echo "Scan multiple repositories with vip-go-compatibility-scanner and note issues found"
 	echo ""
-	echo "Usage: $0 github-organisation github-repos github-token github-labels github-issue-title github-issue-body-intro github-issue-body-end phpcs-standard phpcs-runtime-set"
+	echo "Usage: $0 github-organisation github-repos github-token github-labels github-issue-title github-issue-body github-issue-assign phpcs-standard phpcs-runtime-set"
 	echo ""
 	echo "          github-organisation: GitHub organisation repositories belong to"
 	echo "          github-repos: Comma separated string of repositories to scan"
 	echo "          github-token: Access token for GitHub"
  	echo "          github-labels: Labels to apply to issues created. Separate by commas for multiple"
 	echo "          github-issue-title: Title to use for created issues"
-	echo "          github-issue-body-intro: Some introductionary text for each GitHub issue created"
-	echo "          github-issue-body-end: Ending remarks for each GitHub issue created"
+	echo "          github-issue-body: Explanatory text for each GitHub issue created, include %error_msg% for list of problems"
+	echo "          github-issue-assign: Assign to type of admins: all, direct or outside"
 	echo "          phpcs-standards: PHPCS standard(s) to use while scanning. Separate by commas for multiple"
 	echo "          phpcs-runtime-set: Runtime parameter to set, for example: \"testVersion 7.2-\""
 	echo ""
@@ -34,8 +34,8 @@ echo "-- GitHub organisation: $GITHUB_ORGANISATION"
 echo "    --    repositories: $GITHUB_REPOS"
 echo "    --    labels to apply: $GITHUB_LABELS"
 echo "    --    issue title to use: $GITHUB_ISSUE_TITLE"
-echo "    --    issue body start: $GITHUB_ISSUE_BODY_INTRO"
-echo "    --    issue body end: $GITHUB_ISSUE_BODY_END"
+echo "    --    issue body: $GITHUB_ISSUE_BODY"
+echo "    --    issue assign to admins: $GITHUB_ISSUE_ASSIGN"
 echo "-- PHPCS standard(s) to use: $PHPCS_STANDARDS"
 echo "    --   runtime set: $PHPCS_RUNTIME_SET"
 echo ""
@@ -64,7 +64,7 @@ for REPO_NAME in $(echo "$GITHUB_REPOS") ; do
 	git clone "git@github.com:$GITHUB_ORGANISATION/$REPO_NAME.git" && \
 	echo "Running scanner..." && \
 	popd && \
-	./compatibility-scanner.php --repo-owner="$GITHUB_ORGANISATION" --repo-name="$REPO_NAME" --token="$GITHUB_TOKEN" --github-issue-title="$GITHUB_ISSUE_TITLE" --github-issue-body-intro="$GITHUB_ISSUE_BODY_INTRO" --github-issue-body-end="$GITHUB_ISSUE_BODY_END" --local-git-repo="$TEMP_DIR/$REPO_NAME" --phpcs-path="$HOME/vip-go-ci-tools/phpcs/bin/phpcs" --phpcs-standard="$PHPCS_STANDARDS"  --vipgoci-path="$HOME/vip-go-ci-tools/vip-go-ci/" --phpcs-runtime-set="$PHPCS_RUNTIME_SET" --github-labels="$GITHUB_LABELS" | tee "$TEMP_LOG_FILE" && \
+	./compatibility-scanner.php --repo-owner="$GITHUB_ORGANISATION" --repo-name="$REPO_NAME" --token="$GITHUB_TOKEN" --github-issue-title="$GITHUB_ISSUE_TITLE" --github-issue-body="$GITHUB_ISSUE_BODY" --github-issue-assign="$GITHUB_ISSUE_ASSIGN" --local-git-repo="$TEMP_DIR/$REPO_NAME" --phpcs-path="$HOME/vip-go-ci-tools/phpcs/bin/phpcs" --phpcs-standard="$PHPCS_STANDARDS"  --vipgoci-path="$HOME/vip-go-ci-tools/vip-go-ci/" --phpcs-runtime-set="$PHPCS_RUNTIME_SET" --github-labels="$GITHUB_LABELS" | tee "$TEMP_LOG_FILE" && \
 	echo "Processing of $REPO_NAME done." && \
 	rm -rf "$TEMP_DIR/$REPO_NAME" && \
 	echo "Removed cloned repository from $TEMP_DIR" && \
