b714059278762418d608bbb3f305c1bcadc7ae7a
Gudmundur Haraldsson <gdh1@hi.is>
2020-08-18 15:04:54 +0000

    Add branch parameter

--- scan-multiple-repositories.sh
+++ scan-multiple-repositories.sh
@@ -9,11 +9,12 @@ GITHUB_ISSUE_BODY="$6"
 GITHUB_ISSUE_ASSIGN="$7"
 PHPCS_STANDARDS="$8"
 PHPCS_RUNTIME_SET="$9"
+GIT_BRANCH="${10}"
 
 if [ "" == "$GITHUB_ORGANISATION" ] || [ "" == "$GITHUB_REPOS" ] || [ "" == "$GITHUB_TOKEN" ] || [ "" == "$GITHUB_LABELS" ] || [ "" == "$GITHUB_ISSUE_TITLE" ]|| [ "" == "$GITHUB_ISSUE_BODY" ]|| [ "" == "$GITHUB_ISSUE_ASSIGN" ]  || [ "" == "$PHPCS_STANDARDS" ] || [ "" == "$PHPCS_RUNTIME_SET" ] ; then
 	echo "Scan multiple repositories with vip-go-compatibility-scanner and note issues found"
 	echo ""
-	echo "Usage: $0 github-organisation github-repos github-token github-labels github-issue-title github-issue-body github-issue-assign phpcs-standard phpcs-runtime-set"
+	echo "Usage: $0 github-organisation github-repos github-token github-labels github-issue-title github-issue-body github-issue-assign phpcs-standard phpcs-runtime-set git-branch"
 	echo ""
 	echo "          github-organisation: GitHub organisation repositories belong to"
 	echo "          github-repos: Comma separated string of repositories to scan"
@@ -24,10 +25,14 @@ if [ "" == "$GITHUB_ORGANISATION" ] || [ "" == "$GITHUB_REPOS" ] || [ "" == "$GI
 	echo "          github-issue-assign: Assign to type of admins: all, direct or outside"
 	echo "          phpcs-standards: PHPCS standard(s) to use while scanning. Separate by commas for multiple"
 	echo "          phpcs-runtime-set: Runtime parameter to set, for example: \"testVersion 7.2-\""
+	echo "          git-branch: Git branch to check out. 'master' is default."
 	echo ""
 	exit 1
 fi
 
+if [ "" == "$GIT_BRANCH" ] ; then
+	GIT_BRANCH="master"
+fi
 
 echo "Preparing to process multiple repositories with vip-go-compatibility-scanner:"
 echo "-- GitHub organisation: $GITHUB_ORGANISATION"
@@ -38,6 +43,7 @@ echo "    --    issue body: $GITHUB_ISSUE_BODY"
 echo "    --    issue assign to admins: $GITHUB_ISSUE_ASSIGN"
 echo "-- PHPCS standard(s) to use: $PHPCS_STANDARDS"
 echo "    --   runtime set: $PHPCS_RUNTIME_SET"
+echo "-- Git branch: $GIT_BRANCH"
 echo ""
 echo -n "Do you want to continue? (Y/N) "
 
@@ -62,6 +68,9 @@ for REPO_NAME in $(echo "$GITHUB_REPOS") ; do
 	pushd "$TEMP_DIR" && \
 	echo "Cloning repository from GitHub..." && \
 	git clone "git@github.com:$GITHUB_ORGANISATION/$REPO_NAME.git" && \
+	cd $REPO_NAME && \
+	git checkout "$GIT_BRANCH" && \
+	cd .. && \
 	echo "Running scanner..." && \
 	popd && \
 	./compatibility-scanner.php --repo-owner="$GITHUB_ORGANISATION" --repo-name="$REPO_NAME" --token="$GITHUB_TOKEN" --github-issue-title="$GITHUB_ISSUE_TITLE" --github-issue-body="$GITHUB_ISSUE_BODY" --github-issue-assign="$GITHUB_ISSUE_ASSIGN" --local-git-repo="$TEMP_DIR/$REPO_NAME" --phpcs-path="$HOME/vip-go-ci-tools/phpcs/bin/phpcs" --phpcs-standard="$PHPCS_STANDARDS"  --vipgoci-path="$HOME/vip-go-ci-tools/vip-go-ci/" --phpcs-runtime-set="$PHPCS_RUNTIME_SET" --github-labels="$GITHUB_LABELS" | tee "$TEMP_LOG_FILE" && \
