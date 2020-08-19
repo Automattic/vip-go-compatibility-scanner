8dfa1f679a6c63277942093c751237337ab6db48
Gudmundur Haraldsson <gdh1@hi.is>
2020-08-19 20:16:06 +0000

    CSV functionality import

---
+++ csv-data.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * Get CSV data. First line of file
+ * should be columns.
+ */
+function vipgocs_csv_parse_data(
+	$zendesk_csv_file_path
+) {
+	vipgoci_log(
+		'Parsing CSV data file',
+		array(
+			'zendesk_csv_file_path'	=> $zendesk_csv_file_path,
+		)
+	);
+
+	ini_set( 'auto_detect_line_endings', false );
+
+	$res = array();
+
+	$f = fopen(
+		$zendesk_csv_file_path,
+		'r'
+	);
+
+	if ( ! $f ) {
+		vipgoci_sysexit(
+			'Unable to parse CSV file',
+			array(
+				'zendesk_csv_file_path' => $zendesk_csv_file_path,
+			)
+		);
+	}
+
+	$first = fgetcsv(
+		$f,
+		0,
+		','
+	);
+
+	if ( empty( $first ) ) {
+		vipgoci_sysexit(
+			'CSV file seems empty? First line should be columns.',
+		);
+	}
+
+	while ( $item = fgetcsv(
+		$f,
+		0,
+		','
+	) ) {
+		if ( count( $item ) <= 0 ) {
+			continue;
+		}
+
+		$i = 0;
+
+		$item_arr = array();
+
+		foreach( array_values( $first ) as $key ) {
+			if ( ! isset( $item[ $i ] ) ) {
+				continue;
+			}
+
+			if ( empty( $item[ $i ] ) ) {
+				continue;
+			}
+
+			$item_arr[ $key ] = $item[ $i++ ];
+		}
+
+		if ( ! empty( $item_arr ) ) {
+			$res[] = $item_arr;
+		}
+	}
+
+	fclose(
+		$f
+	);
+
+	ini_set( 'auto_detect_line_endings', false );
+
+	return $res;
+}
+
+/*
+ * Try to match email address with repository.
+ */
+function vipgocs_csv_get_email_for_repo(
+	$csv_data_arr,
+	$repo_owner,
+	$repo_name
+) {
+	$repo_owner = strtolower(
+		$repo_owner
+	);
+
+	$repo_name = strtolower(
+		$repo_name
+	);
+
+	foreach(
+		$csv_data_arr as $csv_item
+	) {
+		if ( 
+			$csv_item['source_repo'] !==
+			$repo_owner .'/' . $repo_name
+		) {
+			continue;
+		}
+
+		return $csv_item['client_email'];
+	}
+
+	return null;
+}
