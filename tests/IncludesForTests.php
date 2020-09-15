1d66f32abc9b722670cec9361e3cc1cb89c541ee
Gudmundur Haraldsson <gdh1@hi.is>
2020-09-15 16:11:13 +0000

    Include vip-go-ci and vip-go-compatibility-scanner when starting scanning

---
+++ tests/IncludesForTests.php
@@ -0,0 +1,28 @@
+<?php
+
+declare( strict_types=1 );
+
+
+if ( ! defined( 'VIPGOCS_UNIT_TESTING' ) ) {
+	define( 'VIPGOCS_UNIT_TESTING', true );
+}
+
+if ( ! defined( 'VIPGOCI_INCLUDED' ) ) {
+	define( 'VIPGOCI_INCLUDED', true );
+}
+
+
+$vipgoci_include_path = getenv( 'VIPGOCI_PATH' );
+
+if (
+	( empty( $vipgoci_include_path ) ) ||
+	( is_dir( $vipgoci_include_path ) === false )
+) {
+	echo 'Error: Path to vip-go-ci is not set or invalid. Specify path using the VIPGOCI_PATH environmental variable.' . PHP_EOL;
+	exit( 255 );
+}
+
+require_once( $vipgoci_include_path . '/main.php' );
+
+require_once( __DIR__ . '/../compatibility-scanner.php' );
+
