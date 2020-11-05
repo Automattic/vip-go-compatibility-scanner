29c6c44843df540fb5892dd7bd8435a897a1abfe
Gudmundur Haraldsson <gdh1@hi.is>
2020-11-05 16:39:25 +0000

    Submodule support: Adding unit-tests

--- tests/OpenIssuesOpenIssuesTest.php
+++ tests/OpenIssuesOpenIssuesTest.php
@@ -97,6 +97,9 @@ final class OpenIssuesOpenIssuesTest extends TestCase {
 								'line' => 2,
 								'column' => 14,
 								'file_path' => 'file1.php',
+								'github_commit_url' => 'https://github.com/mytestuseraccount0x900/testrepo18736/blob/8171',
+								'file_is_in_submodule' => false,
+								'file_path_without_submodule' => 'file1.php',
 							)
 						)
 					),
@@ -117,9 +120,32 @@ final class OpenIssuesOpenIssuesTest extends TestCase {
 								'line' => 3,
 								'column' => 14,
 								'file_path' => 'file3.php',
+								'github_commit_url' => 'https://github.com/mytestuseraccount1x900/testrepo19936/blob/9300',
+								'file_is_in_submodule' => false,
+								'file_path_without_submodule' => 'file3.php',
 							)
 						)
-					)
+					),
+
+					'file4.php' => array(
+						'messages' => array(
+							array(
+								'message' => "All output should be run through an escaping function (see the Security sections in the WordPress Developer Handbooks), found 'time'.",
+								'source' => 'WordPress.Security.EscapeOutput.OutputNotEscaped',
+								'severity' => 5,
+								'fixable' => false,
+								'type' => 'ERROR',
+								'line' => 30,
+								'column' => 14,
+								'file_path' => 'sub-module-folder/file4.php',
+								'github_commit_url' => 'https://github.com/yetanothertestaccount1x900/testrepo39999/blob/18900',
+								'file_is_in_submodule' => true,
+								'file_path_without_submodule' => 'file4.php',
+							)
+						)
+					),
+
+
 				),
 				'warnings' => 0,
 				'errors' => 2,
@@ -138,15 +164,17 @@ final class OpenIssuesOpenIssuesTest extends TestCase {
 
 		$this->assertEquals(
 			array(
-				'issues_found' => 2,
-				'issues_opened' => 2,
+				'issues_found' => 3,
+				'issues_opened' => 3,
 			),
 			$issue_statistics,
 		);
 
 		$this->assertEquals(
-			'{"_POST":{"{\"title\":\"my-issue-title1file1_php\",\"body\":\"my-issue-body1,_\\\\n*_<b>Error<\\\\\\/b>:_All_output_should_be_run_through_an_escaping_function_(see_the_Security_sections_in_the_WordPress_Developer_Handbooks),_found_\'time\'__https:\\\\\/\\\\\/github_com\\\\\\/test\\\\\\/test\\\\\\/blob\\\\\\/abcd1234\\\\\\/file1_php#L2\\\\n\\\\n\\\\n\\\\n,_main-branch\\\\n\",\"assignees\":":{"\"my-username1\",\"my-username2\"":""}},"_GET":[]}' .
-				'{"_POST":{"{\"title\":\"my-issue-title1file3_php\",\"body\":\"my-issue-body1,_\\\\n*_<b>Error<\\\\\\/b>:_All_output_should_be_run_through_an_escaping_function_(see_the_Security_sections_in_the_WordPress_Developer_Handbooks),_found_\'time\'__https:\\\\\\/\\\\\\/github_com\\\\\\/test\\\\\\/test\\\\\\/blob\\\\\\/abcd1234\\\\\\/file3_php#L3\\\\n\\\\n\\\\n\\\\n,_main-branch\\\\n\",\"assignees\":":{"\"my-username1\",\"my-username2\"":""}},"_GET":[]}',
+			'{"_POST":{"{\"title\":\"my-issue-title1file1_php\",\"body\":\"my-issue-body1,_\\\\n*_<b>Error<\\\\\\/b>:_All_output_should_be_run_through_an_escaping_function_(see_the_Security_sections_in_the_WordPress_Developer_Handbooks),_found_\'time\'__https:\\\\\/\\\\\/github_com\\\\\\/mytestuseraccount0x900\\\\\\/testrepo18736\\\\\\/blob\\\\\\/8171\\\\\\/file1_php#L2\\\\n\\\\n\\\\n\\\\n,_main-branch\\\\n\",\"assignees\":":{"\"my-username1\",\"my-username2\"":""}},"_GET":[]}' .
+				'{"_POST":{"{\"title\":\"my-issue-title1file3_php\",\"body\":\"my-issue-body1,_\\\\n*_<b>Error<\\\\\\/b>:_All_output_should_be_run_through_an_escaping_function_(see_the_Security_sections_in_the_WordPress_Developer_Handbooks),_found_\'time\'__https:\\\\\\/\\\\\\/github_com\\\\\\/mytestuseraccount1x900\\\\\\/testrepo19936\\\\\\/blob\\\\\\/9300\\\\\\/file3_php#L3\\\\n\\\\n\\\\n\\\\n,_main-branch\\\\n\",\"assignees\":":{"\"my-username1\",\"my-username2\"":""}},"_GET":[]}' .
+				'{"_POST":{"{\"title\":\"my-issue-title1file4_php\",\"body\":\"my-issue-body1,_\\\\n*_<b>Error<\\\\\\/b>:_All_output_should_be_run_through_an_escaping_function_(see_the_Security_sections_in_the_WordPress_Developer_Handbooks),_found_\'time\'___In_submodule,_<a_href":"\\\\\\"https:\\\\\\/\\\\\\/github.com\\\\\\/yetanothertestaccount1x900\\\\\\/testrepo39999\\\\\\/blob\\\\\\/18900\\\\\\/file4.php#L30\\\\\\">here<\\\\\\/a>.\\\\n\\\\n\\\\n\\\\n, main-branch\\\\n\",\"assignees\":[\"my-username1\",\"my-username2\"]}"},"_GET":[]}',
+
 			file_get_contents(
 				$this->output_file
 			)
