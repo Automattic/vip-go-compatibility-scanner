cb8a9ccec0abae034a58a2bc6a508e8e157723d8
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2021-06-29 14:53:34 +0000

    Add requires file.

---
+++ requires.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Require all code needed.
+ */
+require_once( __DIR__ . '/github-api.php' );
+require_once( __DIR__ . '/scan-files.php' );
+require_once( __DIR__ . '/open-issues.php' );
+require_once( __DIR__ . '/zendesk-api.php' );
+require_once( __DIR__ . '/csv-data.php' );
+require_once( __DIR__ . '/options.php' );
+require_once( __DIR__ . '/zendesk-db.php' );
+require_once( __DIR__ . '/utils.php' );
+require_once( __DIR__ . '/compatibility-scanner.php' );
+
