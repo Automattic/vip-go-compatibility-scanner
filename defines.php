d6c9b00ac00bb53e4a75bff820d598ba43d0b727
Gudmundur D. Haraldsson <gudmdharalds@users.noreply.github.com>
2022-10-21 11:59:31 +0000

    Update defines.php

--- defines.php
+++ defines.php
@@ -3,3 +3,5 @@
 define( 'VIPGOCS_PHPCS_CACHEDB_TABLE_NAME', 'vipgocs_phpcs_cachedb' );
 define( 'VIPGOCS_PHPCS_CACHEDB_INDEX_NAME', 'vipgocs_phpcs_cachedb_index' );
 define( 'VIPGOCS_PHPCS_SOURCE_INTERNAL',    'Vipgocs.Internal' );
+
+define( 'VIPGOCI_HTTP_API_WAIT_TIME_SECONDS', 5 );
